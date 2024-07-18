2f0f9cc65ce58d42f364822381ebd9e90b59bca2
KrLite <68179735+KrLite@users.noreply.github.com>
2024-07-18 16:52:40 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -73,7 +73,7 @@ publishing {
 	repositories {
 		maven {
 			name = "GitHubPackages"
-			url = uri("https://maven.pkg.github.com/KessokuTeaTime/${rootProject.name}")
+			url = uri("https://maven.pkg.github.com/KessokuTeaTime/${libs.versions.mod}")
 			credentials {
 				username = System.getenv("GITHUB_ACTOR")
 				password = System.getenv("GITHUB_TOKEN")
